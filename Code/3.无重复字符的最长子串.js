3c8021b4eb5d4889a273a2f2a5001a0375b21100
sinian <1198246001@qq.com>
2021-08-02 17:09:09 +0800

    C

--- Code/3.无重复字符的最长子串.js
+++ Code/3.无重复字符的最长子串.js
@@ -10,7 +10,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-
+    let max = 0, arr = [];
+    for (let i = 0; i < s.length; i++) {
+        let index = arr.indexOf(s[i]);
+        if (index !== -1) {
+            arr.splice(0, index + 1); // 碰到重复的值删除到重复值所处的位置 如abcb -> cb
+        }
+        arr.push(s.charAt(i)); // arr缓存值
+        max = Math.max(max, arr.length);
+    }
+    return max;
 };
 // @lc code=end
 
