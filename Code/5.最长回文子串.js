d14b2c821313304980f41b25e200b61830fb5e5b
sinian <1198246001@qq.com>
2021-08-03 15:04:16 +0800

    Content Update

--- Code/5.最长回文子串.js
+++ Code/5.最长回文子串.js
@@ -9,8 +9,22 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-
+// 暴力解法不可取
+var longestPalindrome = function (s) {
+    let max = 0, tempArr = [], tempStr = "";
+    for (let i = 0; i < s.length; i++) {
+        for (let j = s.length; j > i; j--) {
+            tempArr.push(s[i] + s.substring(i + 1, j))
+        }
+    }
+    for (let item of tempArr) { 
+        if (item.split('').reverse().join('') === item) {
+            tempStr = item.length > max ? item : tempStr;
+            max = Math.max(item.length, max);
+        }
+    }
+    return tempStr;
 };
+// 
 // @lc code=end
 
